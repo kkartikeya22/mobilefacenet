91d42ded34fe98996aa262ad5a2b6097e112a6bc
kkartikeya22 <kkartikeya22@iitk.ac.in>
2024-05-30 23:32:14 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,15 @@
+BATCH_SIZE = 512
+SAVE_FREQ = 1
+TEST_FREQ = 1
+TOTAL_EPOCH = 7
+
+RESUME = ''
+SAVE_DIR = './model'
+MODEL_PRE = 'CASIA_B512_'
+
+
+CASIA_DATA_DIR = '/content/casia_webface/CASIA'
+LFW_DATA_DIR = '/content/lfw'
+
+GPU = 0, 1
+
