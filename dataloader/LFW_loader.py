f79211a02b123650b4f2dfcdd37ba8786e37188c
kkartikeya22 <kkartikeya22@iitk.ac.in>
2024-05-31 00:04:32 +0530

    ffff

--- dataloader/LFW_loader.py
+++ dataloader/LFW_loader.py
@@ -1,18 +1,17 @@
 import numpy as np
-import scipy.misc
-
+import imageio
 import torch
+
 class LFW(object):
     def __init__(self, imgl, imgr):
-
         self.imgl_list = imgl
         self.imgr_list = imgr
 
     def __getitem__(self, index):
-        imgl = scipy.misc.imread(self.imgl_list[index])
+        imgl = imageio.imread(self.imgl_list[index])
         if len(imgl.shape) == 2:
             imgl = np.stack([imgl] * 3, 2)
-        imgr = scipy.misc.imread(self.imgr_list[index])
+        imgr = imageio.imread(self.imgr_list[index])
         if len(imgr.shape) == 2:
             imgr = np.stack([imgr] * 3, 2)
 
@@ -27,7 +26,6 @@ class LFW(object):
 
     def __len__(self):
         return len(self.imgl_list)
-
 
 if __name__ == '__main__':
     pass
